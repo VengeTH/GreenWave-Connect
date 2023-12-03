cc92c17744baf74b4ca21b46eb79b3611256d00c
VengeTH <96782670+VengeTH@users.noreply.github.com>
2023-12-03 23:53:53 +0800

    will change the background image

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-const observer = new IntersectionObserver(
-    (items)
-)
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+const containersz = document.getElementById('wholediv');
+left.addEventListener('click', () => {
+    // Move the last div to the beginning to rotate them
+    const tempHTML = div1.innerHTML;
+    div1.innerHTML = div3.innerHTML;
+    div3.innerHTML = div2.innerHTML;
+    div2.innerHTML = tempHTML;
+});
+right.addEventListener('click', () => {
+    // Move the first div to the end to rotate them
+    const tempHTML = div1.innerHTML;
+    div1.innerHTML = div2.innerHTML;
+    div2.innerHTML = div3.innerHTML;
+    div3.innerHTML = tempHTML;
+});
