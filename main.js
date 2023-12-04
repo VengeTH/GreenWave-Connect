004dee20fd6470213af756ed59eb72382a4bb1b2
VengeTH <96782670+VengeTH@users.noreply.github.com>
2023-12-04 11:00:05 +0800

    committed

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-const abtus = document.getElementById("abtusz")
-const options = {
-    rootMargin: "0px",
-    threshold: 0.25
+// const abtus = document.getElementById("abtusz")
+// const options = {
+//     rootMargin: "0px",
+//     threshold: 0.25
+// }
+
+// function observeFunc(entries){
+//     entries.forEach(entry => {
+//         console.log(entry)
+//         if(entry.isIntersecting){
+
+//         }
+//     })
+// }
+
+// const observer = new IntersectionObserver(function(entries, observer){
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting){
+//             abtus.classList.add("abtus")
+//         }
+//     })
+// }, options)
+
+function lefted(){
+    var img = document.getElementById(volunteer);
+    var tempSrc = img.src;
+    var img2 =
 }
-
-function observeFunc(entries){
-    entries.forEach(entry => {
-        console.log(entry)
-        if(entry.isIntersecting){
-
-        }
-    })
-}
-
-const observer = new IntersectionObserver(function(entries, observer){
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            abtus.classList.add("abtus")
-        }
-    })
-}, options)
