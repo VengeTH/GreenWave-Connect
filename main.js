24a7ada2a6d00905a256651e70b5785187a6140d
VengeTH <96782670+VengeTH@users.noreply.github.com>
2023-12-04 18:41:42 +0800

    about us :D

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-// const abtus = document.getElementById("abtusz")
-// const options = {
-//     rootMargin: "0px",
-//     threshold: 0.25
-// }
-
-// function observeFunc(entries){
-//     entries.forEach(entry => {
-//         console.log(entry)
-//         if(entry.isIntersecting){
-
-//         }
-//     })
-// }
-
-// const observer = new IntersectionObserver(function(entries, observer){
-//     entries.forEach(entry => {
-//         if(entry.isIntersecting){
-//             abtus.classList.add("abtus")
-//         }
-//     })
-// }, options)
-
-function lefted(){
-    var img = document.getElementById(volunteer);
-    var tempSrc = img.src;
-    var img2 =
+const abtus = document.getElementById("abtusz")
+const options = {
+    rootMargin: "0px",
+    threshold: 0.25
 }
+
+function observeFunc(entries){
+    entries.forEach(entry => {
+        console.log(entry)
+        if(entry.isIntersecting){
+
+        }
+    })
+}
+
+const observer = new IntersectionObserver(function(entries, observer){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            abtus.classList.add("abtus")
+        }
+    })
+}, options)
+
+observer.observe(abtus)
+// function lefted(){
+//     var img = document.getElementById(volunteer);
+//     var tempSrc = img.src;
+//     var img2 =
+// }
