af0b8e27090ca7d8f0ac0be522d4724487316d18
VengeTH <96782670+VengeTH@users.noreply.github.com>
2023-12-04 10:03:11 +0800

    currently fixing the header!!

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-const left = document.getElementById('left');
-const right = document.getElementById('right');
-const containersz = document.getElementById('wholediv');
-left.addEventListener('click', () => {
-    // Move the last div to the beginning to rotate them
-    const tempHTML = div1.innerHTML;
-    div1.innerHTML = div3.innerHTML;
-    div3.innerHTML = div2.innerHTML;
-    div2.innerHTML = tempHTML;
-});
-right.addEventListener('click', () => {
-    // Move the first div to the end to rotate them
-    const tempHTML = div1.innerHTML;
-    div1.innerHTML = div2.innerHTML;
-    div2.innerHTML = div3.innerHTML;
-    div3.innerHTML = tempHTML;
-});
+const abtus = document.getElementById("abtusz")
+const options = {
+    rootMargin: "0px",
+    threshold: 0.25
+}
+
+function observeFunc(entries){
+    entries.forEach(entry => {
+        console.log(entry)
+        if(entry.isIntersecting){
+
+        }
+    })
+}
+
+const observer = new IntersectionObserver(function(entries, observer){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            abtus.classList.add("abtus")
+        }
+    })
+}, options)
