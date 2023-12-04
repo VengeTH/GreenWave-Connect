ec3922606260842498f410d67b94943917a4f9a8
VengeTH <96782670+VengeTH@users.noreply.github.com>
2023-12-04 21:23:56 +0800

    added backgrounds to other divs

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-const abtus = document.getElementById("abtusz")
-const options = {
-    rootMargin: "0px",
-    threshold: 0.25
-}
+// const abtus = document.getElementById("abtusz")
+// const options = {
+//     rootMargin: "0px",
+//     threshold: 0.25
+// }
 
-function observeFunc(entries){
-    entries.forEach(entry => {
-        console.log(entry)
-        if(entry.isIntersecting){
+// function observeFunc(entries){
+//     entries.forEach(entry => {
+//         console.log(entry)
+//         if(entry.isIntersecting){
+//             const { target } = entry
 
-        }
-    })
-}
+//             if (
+//                 target.id === "abtusz"
+//             ){
+//                 const abtusbutton = document.getElementById("abtusbutton")
+//                 abtusbutton.style.backgroundColor = "#3d6e48";
+//                 abtusbutton.style.color = "#ffffff";
+//             }
+//         }
+//     })
+// }
 
-const observer = new IntersectionObserver(function(entries, observer){
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            abtus.classList.add("abtus")
-        }
-    })
-}, options)
+// const observer = new IntersectionObserver(function(entries, observer){
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting){
+//             abtus.classList.add("abtus")
+//         }
+//     })
+// }, options)
 
-observer.observe(abtus)
+// observer.observe(abtus)
+const IntersectionObserver =new IntersectionObserver((entries) =>   {
+    if (entries[0].intersectionRatio <= 0) {
+        return;
+    }
+    loadItems(10);
+    console.log("loaded new items");});
+IntersectionObserver.observe(document.querySelector("abtusz"));
 // function lefted(){
 //     var img = document.getElementById(volunteer);
 //     var tempSrc = img.src;
